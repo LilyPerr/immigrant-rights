73eba7f045e2d1b05d6d318bcabdffe16f679246
Teagan Ampe <ampe@usc.edu>
2020-02-01 15:00:40 -0800

    converted html files to php

---
+++ index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+
+    <title>Immigrant Rights</title>
+
+    <style>
+        thead tr th {
+            border-right: solid 2px lightgray;
+        }
+
+        .title {
+            margin-top: 10px;
+            margin-left: 20px;
+            margin-right: 20px;
+            margin-bottom: 20px;
+        }
+
+    </style>
+
+  </head>
+  <body>
+
+    <!-- SHOW JUST INITIAL CATEGORIES; WHEN CLICK, EXPAND OPEN -->
+    <div class="title text-lg-left text-sm-center text-"><h1>Immigrant Rights</h1></div>
+    
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th class="top-header" scope="col" colspan="4">Biographical Info</th>
+                <th class="top-header" scope="col" colspan="2">Preliminary Qs</th>
+                <th class="top-header" scope="col">Last Entry</th>
+                <th class="top-header" scope="col">Community Ties</th>
+                <th class="top-header" scope="col">Detention History</th>
+                <th class="top-header" scope="col">Potential Bars</th>
+                <th class="top-header" scope="col">Other Details</th>
+              </tr>
+              <tr>
+                <!-- biographical info -->
+                <th class="subheading" scope="col">Name</th>
+                <th class="subheading" scope="col">A#</th>
+                <th class="subheading" scope="col">Origin Country</th>
+                <th class="subheading" scope="col">Detention Center</th>
+
+                <!-- preliminary qs -->
+                <th class="subheading" scope="col">Previous Attorney?</th>
+                <th class="subheading">Test</th>
+                
+                <!-- last entry -->
+                <th class="subheading" scope="col">Last US Entry</th>
+                
+                <!-- community ties -->
+                <th class="subheading" scope="col">U.S. Family</th>
+
+                <!-- detention history -->
+                <th class="subheading" scope="col">How Detained?</th>
+
+                <!-- potential bars -->
+                <th class="subheading" scope="col">Previously Deported?</th>
+
+                <!-- other details -->
+                <th class="subheading" scope="col">Anything Else?</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row"><a href="details.php?id=">Teagan Ampe</a></th>
+                <td>Canada</td>
+                <td>201-942-292</td>
+                <td>[sdlkjf]</td>
+                <td>No</td>
+                <td>[]</td>
+                <td>02/01/2020</td>
+                <td>Mother</td>
+                <td>Traffic Stop</td>
+                <td>No</td>
+                <td>here are some more details that i'm writing to test how the table looks when there's a lot of text</td>
+              </tr>
+              <tr>
+                <th scope="row"><a href="details.php?id=">Lily Perry</a></th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@fat</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@fat</td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="row">Lily Perry</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@fat</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@fat</td>
+                <td></td>
+                <td></td>
+              </tr>
+              
+            </tbody>
+        </table>
+    </div>
+    
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
