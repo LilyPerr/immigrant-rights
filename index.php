b4feae8a0ca696702a29fa224ba1737107e587a4
Teagan Ampe <ampe@usc.edu>
2020-02-01 22:45:33 -0800

    changed titles to ibridge

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
 
-    <title>Immigrant Rights</title>
+    <title>iBridge</title>
 
     <style>
         thead tr th {
@@ -52,7 +52,7 @@
   <body>
 
     <!-- SHOW JUST INITIAL CATEGORIES; WHEN CLICK, EXPAND OPEN -->
-    <div class="title text-lg-left text-sm-center text-"><h1>Immigrant Rights</h1></div>
+    <div class="title text-lg-left text-sm-center text-"><h1>iBridge</h1></div>
     
     <div class="table-responsive">
         <table class="table table-striped">
